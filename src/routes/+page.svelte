fc670eb69eacdfe8b555a07902a548e3ab57d178
Everton Buzzi <evertonbuzzi@gmail.com>
2023-05-13 13:54:17 +0100

    #EB: Svelte with beercss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="center-align">
+    <h5 class="center-align">Welcome to SvelteKit with Beercss</h5>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <button>Svelte</button>
+</div>
+
