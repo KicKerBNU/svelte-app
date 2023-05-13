a079047c2b7a7cc54ddd1ff6b8064c84b689150e
Everton Buzzi <evertonbuzzi@gmail.com>
2023-05-13 14:10:14 +0100

    #EB: Svelte tests with ui from Beercss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,14 @@
     <h5 class="center-align">Welcome to SvelteKit with Beercss</h5>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
     <button>Svelte</button>
+    <button data-ui="#modal">Modal Default</button>
+    <nav class="wrap modal" id="modal">
+      <h5>Default modal</h5>
+        <div>Some text here</div>
+        <nav class="right-align">
+          <button class="border">Cancel</button>
+          <button>Confirm</button>
+        </nav>
+    </nav>
 </div>
 
