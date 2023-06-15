be073ee04b01a0ca7d7d089ed04e8bee6b1cbaae
Everton Buzzi <evertonbuzzi@gmail.com>
2023-06-15 10:19:44 +0100

    #EB: Importing beercss from node_modules in app.hmtl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
-<script>
-  import "../app.css";
-  import "../app.js";
-</script>
+
 <div class="center-align">
     <h5 class="center-align">Welcome to SvelteKit with Beercss</h5>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
